e1ce3e9a21a51d9aef7b18766de941864c6ada6f
James Zhang <zhang_james@icloud.com>
2022-11-12 22:59:08 -0800

    npm start working, popup library added

--- uc-lavatory/src/index.js
+++ uc-lavatory/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Popup></Popup>
   </React.StrictMode>
 );
 
