9847907ffc6cfb7af3fb329593429e74a1f137bd
EthanDao4444 <ethandao4444@gmail.com>
2022-11-24 07:35:29 -0800

    Created test dropdowns for each of the pages for each of the halls we are choosing to display. Also changed "homepage" in package.json so it defaults to "/" instead of "UC-lavatory".

--- uc-lavatory/src/pages/maps/BoelterHall.js
+++ uc-lavatory/src/pages/maps/BoelterHall.js
@@ -1,9 +1,28 @@
 import React from 'react';
 
 const BoelterHall = () => {
+    const [value, setValue] = React.useState("floor1")
+    const handleChange = (event) => (
+        setValue(event.target.value)
+    );
     return (
         <div>
-            <p>boelter hall data</p>
+            <h1>Boelter Hall Data</h1>
+            <label>
+                Please select floor.
+                <select value={value} onChange={handleChange}>
+                    <option value = "floor1">Floor 1</option>
+                    <option value = "floor2">Floor 2</option>
+                    <option value = "floor3">Floor 3</option>
+                    <option value = "floor4">Floor 4</option>
+                    <option value = "floor5">Floor 5</option>
+                    <option value = "floor6">Floor 6</option>
+                    <option value = "floor7">Floor 7</option>
+                    <option value = "floor8">Floor 8</option>
+                    <option value = "floor9">Floor 9</option>
+                </select>
+            </label>
+            <p>Will display data for {value} when this is finished.</p>
         </div>
     );
 }
--- uc-lavatory/src/pages/maps/SchoenbergHall.js
+++ uc-lavatory/src/pages/maps/SchoenbergHall.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
 const SchoenbergHall = () => {
+    const [value, setValue] = React.useState("floor1")
+    const handleChange = (event) => (
+        setValue(event.target.value)
+    );
     return (
         <div>
-            <p>schoenberg hall page</p>
+            <h1>Schoenberg Hall Data</h1>
+            <label>
+                Please select floor.
+                <select value={value} onChange={handleChange}>
+                    <option value = "floor1">Floor 1</option>
+                    <option value = "floor2">Floor 2</option>
+                </select>
+            </label>
+            <p>Will display data for {value} when this is finished.</p>
         </div>
     );
 }
