f8f32d859fa5d71ddcde6b1ed063f82d16df290c
EthanDao4444 <ethandao4444@gmail.com>
2022-11-24 12:01:13 -0800

    Adds sound and images to Homepage.js. Also added some basic animation to images.

--- uc-lavatory/src/pages/Homepage.js
+++ uc-lavatory/src/pages/Homepage.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import timmy from '../timmy/timmy.jpg';
+import timmyvoice from '../timmy/timmysound.mp3';
+import "./Homepage.css";
+
+let audio = new Audio(timmyvoice)
+  
+const start = () => {
+    audio.play()
+}
 
 const HomePage = () => {
     return (
             <div>
                 <h1>Top Restrooms</h1>
+                <img onClick={start} className = "Timmy" src = {timmy} alt = "Timmy the Toilet Buddy"/>
         </div>
     );
 }
