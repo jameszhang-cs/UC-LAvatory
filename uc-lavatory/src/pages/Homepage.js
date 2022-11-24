41bee121090b6fd7a662448cf94cddea92eb29be
EthanDao4444 <ethandao4444@gmail.com>
2022-11-24 07:16:38 -0800

    Rearranged code so that the reviews form is in Reviews.js and began basic setup for the homepage.

--- uc-lavatory/src/pages/Homepage.js
+++ uc-lavatory/src/pages/Homepage.js
@@ -1,19 +1,9 @@
 import React from 'react';
-import Popup from '../components/Popup';
-import { useState } from 'react'
-import Form from '../components/Form';
 
 const HomePage = () => {
-    const [buttonPopup, setButtonPopup] = useState(false);
     return (
             <div>
-            <main>
-                <h1>UC-Lavatories</h1>
-                <button onClick={() => setButtonPopup(true)}>submit review</button>
-            </main>
-            <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
-                <Form></Form>
-            </Popup>
+                <h1>Top Restrooms</h1>
         </div>
     );
 }
--- uc-lavatory/src/pages/Reviews.js
+++ uc-lavatory/src/pages/Reviews.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import Popup from '../components/Popup';
+import { useState } from 'react'
+import Form from '../components/Form';
 
-const Reviews = () => {
+const HomePage = () => {
+    const [buttonPopup, setButtonPopup] = useState(false);
     return (
-        <div>
+            <div>
+            <main>
+                <h1>UC-Lavatories</h1>
+                <button onClick={() => setButtonPopup(true)}>submit review</button>
+            </main>
+            <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+                <Form></Form>
+            </Popup>
+            <h1>See reviews for each hall</h1>
             <ul id ='nav-list'>
-                <li><a href = "http://localhost:3000/boelterhall">Boelter Hall</a></li>
-                <li><a href = "http://localhost:3000/schoenberghall">Schoenberg Hall</a></li>
+                <a href = "http://localhost:3000/boelterhall">Boelter Hall</a>
+                <br></br>
+                <a href = "http://localhost:3000/schoenberghall">Schoenberg Hall</a>
             </ul>
         </div>
     );
 }
 
-export default Reviews;
+export default HomePage;
+
