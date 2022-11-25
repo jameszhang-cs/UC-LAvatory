ef09fe7ca90e5dbda2a7b8230d527aa055cf815f
marianne <tsayenn@gmail.com>
2022-11-25 00:55:09 -0800

    Made slideshow banner with pictures of UCLA for homepage.

--- uc-lavatory/src/pages/Homepage.js
+++ uc-lavatory/src/pages/Homepage.js
@@ -1,27 +1,30 @@
 import React from 'react';
-import timmy from '../timmy/timmy.jpg';
-import timmyvoice from '../timmy/timmysound.mp3';
 import ucla_pritzker from '../ucla_images/ucla_pritzker.jpg';
 import ucla_royce from '../ucla_images/ucla_royce.jpg';
 import ucla_birdseye from '../ucla_images/ucla_birdseye.jpg';
 import ucla_cos from '../ucla_images/ucla_cos.jpg';
 import "./Homepage.css";
 
-let audio = new Audio(timmyvoice)
-  
-const start = () => {
-    audio.play()
-}
-
 const Homepage = () => {
     return (
-        <div className="Container">
-            <img className="HeroImage1" src={ucla_royce} />
-            <img className="HeroImage2" src={ucla_pritzker} />
-            <img className="HeroImage3" src={ucla_birdseye} />
-            <img className="HeroImage4" src={ucla_cos} />
+        <div className = "Homepage">
+            <div className="Banner">
+                <span className="BannerText">
+                    <h1>UC-LAvatory</h1>
+                    <h2>Insert clever slogan here.</h2>
+                </span>
+                <img className="HeroImage" src={ucla_royce} alt="Royce Hall" />
+                <img className="HeroImage" src={ucla_pritzker} alt="Pritzker Hall" />
+                <img className="HeroImage" src={ucla_birdseye} alt="UCLA Birdseye View"/>
+                <img className="HeroImage" src={ucla_cos} alt="UCLA Court of Sciences" />
+            </div>
+            <main>
+                <br></br> <h1>Most Helpful Reviews</h1>
+                <p>Display clickable squares of reviews here</p>
+            </main>
         </div>
-    );
+    ); 
+
 }
 
 export default Homepage;
