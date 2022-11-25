31693c30ca081bc6622dc90981e5dec54c1c89f1
marianne <tsayenn@gmail.com>
2022-11-24 23:53:36 -0800

    Working banner slideshow, no text

--- uc-lavatory/src/pages/Homepage.js
+++ uc-lavatory/src/pages/Homepage.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import timmy from '../timmy/timmy.jpg';
-import sc_student_store from '../ucla_images/sc_student_store.jpg';
-import ucla from '../ucla_images/ucla.jpg';
 import timmyvoice from '../timmy/timmysound.mp3';
+import ucla_pritzker from '../ucla_images/ucla_pritzker.jpg';
+import ucla_royce from '../ucla_images/ucla_royce.jpg';
+import ucla_birdseye from '../ucla_images/ucla_birdseye.jpg';
+import ucla_cos from '../ucla_images/ucla_cos.jpg';
 import "./Homepage.css";
 
 let audio = new Audio(timmyvoice)
@@ -11,13 +13,16 @@ const start = () => {
     audio.play()
 }
 
-const HomePage = () => {
+const Homepage = () => {
     return (
-            <div>
-                <img className="HeroImages" src = {ucla} />
-            </div>
+        <div className="Container">
+            <img className="HeroImage1" src={ucla_royce} />
+            <img className="HeroImage2" src={ucla_pritzker} />
+            <img className="HeroImage3" src={ucla_birdseye} />
+            <img className="HeroImage4" src={ucla_cos} />
+        </div>
     );
 }
 
-export default HomePage;
+export default Homepage;
 
