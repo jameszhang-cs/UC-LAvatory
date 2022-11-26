d9562f3b1af80a88d80b2f5c6fb3a1a18559a189
EthanDao4444 <ethandao4444@gmail.com>
2022-11-25 22:03:49 -0800

    Began process of filtering out reviews by floor by first being able to check what floor it was.

--- uc-lavatory/src/pages/maps/GenericHall.js
+++ uc-lavatory/src/pages/maps/GenericHall.js
@@ -29,39 +29,35 @@ function DisplayReviews(props) {
 );
 }
 function FloorSelect(props) {
-    const [setValue] = React.useState("floor1")
-    const handleChange = event => {
-        setValue(event.target.value);
-    };
-
-    var arr = Array(1);
-    for (var i = 0; i < props.floors; i++){
-        var obj = {};
-        obj["value"] = "floor" + (i + 1);
-        obj["text"] = "Floor " + (i + 1);
-        arr.push(obj);
-    }    
-    
-      return (
-        <div>
-            <h1>Top Restrooms in this Hall</h1>
-                <label>Sort reviews for this restroom by: {" "}</label>
-                <select>
-                    <option value = "">Please select an option.</option>
-                    <option value = "time">time</option>
-                    <option value = "helpfulness">helpfulness</option>
-                </select>
-                <br></br>
-            <label>Please select floor. {" "}</label>
-          <select onChange={handleChange}>
-            {arr.map((option, index) => (
-              <option key={index} value={option.value}>
-                {option.text}
-              </option>
-            ))}
-          </select>
-        </div>
-      );
+  const [floorToDisplay, changeFloor ] = React.useState(0);
+  var arr = Array(1);
+  for (var i = 0; i < props.floors; i++){
+      var obj = {};
+      obj["value"] = (i + 1);
+      obj["text"] = "Floor " + (i + 1);
+      arr.push(obj);
+  }    
+  
+    return (
+      <div>
+          <h1 >Floor: {floorToDisplay}</h1>
+          <h1>Top Restrooms in this Hall</h1>
+              <label>Sort reviews for this restroom by: {" "}</label>
+              <select>
+                  <option value = "time">time</option>
+                  <option value = "helpfulness">helpfulness</option>
+              </select>
+              <br></br>
+          <label>Please select floor. {" "}</label>
+        <select name = "floorToDisplay" onChange = {e => changeFloor(e.target.value)}>
+          {arr.map((option, index) => (
+            <option key={index} value={option.value}>
+              {option.text}
+            </option>
+          ))}
+        </select>
+      </div>
+    );
 };
 
 const GenericHall = {
