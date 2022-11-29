2b5a0bf48f0793b8a2cdf82a6dc4428e29d18123
Jeffrey <jehfoori@gmail.com>
2022-11-28 20:28:11 -0800

    added dynamic marker that moves on mouse click. Also tracks coordinates

--- uc-lavatory/src/pages/Map.js
+++ uc-lavatory/src/pages/Map.js
@@ -1,11 +1,16 @@
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import buildingData from "./data/buildings.json"
+import { useMapEvents } from 'react-leaflet/hooks'
 import L from 'leaflet';
+import { useState } from 'react'
+import geolib from 'geolib';
 import "./Map.css"
 import "leaflet/dist/leaflet.css";
 
 import icon from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+const center = [34.0689, -118.4452]
+const buildings = buildingData.features
 
 let DefaultIcon = L.icon({
     iconUrl: icon,
@@ -14,14 +19,33 @@ let DefaultIcon = L.icon({
 
 L.Marker.prototype.options.icon = DefaultIcon;
 
+const LocationMarker = () => {
+
+  const [position, setPosition] = useState(null)
+  const map = useMapEvents({
+    click(e) {
+      setPosition(e.latlng)
+      map.flyTo(e.latlng, map.getZoom())
+    },
+  })
+  return position === null ? null : (
+    <Marker position={position}>
+      <Popup>
+        <h1>You are here!</h1>
+        <p>coordinates: {position.lat}, {position.lng}</p>
+      </Popup>
+    </Marker>
+  )
+}
+
 function Map() {
     return (
-      <MapContainer center={[34.0689, -118.4452]} zoom={20} scrollWheelZoom={false}>
+      <MapContainer center={center} zoom={20} scrollWheelZoom={false}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
-  
+        <LocationMarker />
         {buildingData.features.map(building => (
           <Marker key={building.properties.BUILDING_ID} position={[
             building.geometry.coordinates[0],
@@ -39,5 +63,4 @@ function Map() {
       </MapContainer>
     );
   }
-
 export default Map;
