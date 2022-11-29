90312f923ffbbaefde536ee4f85913f90ff11dc3
Jeffrey <jehfoori@gmail.com>
2022-11-29 03:08:02 -0800

    changed dynamic marker to a red color for differentiation. User repositions marker by clicking. Popup now displays the nearest building, as well as the distance the user's marker is away from said building. It seems GeoLib's getPreciseDistance function is still wildly inaccurate for very small distances. Will look into this.

--- uc-lavatory/src/pages/Map.js
+++ uc-lavatory/src/pages/Map.js
@@ -2,40 +2,73 @@ import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import buildingData from "./data/buildings.json"
 import { useMapEvents } from 'react-leaflet/hooks'
 import L from 'leaflet';
-import { useState } from 'react'
-import geolib from 'geolib';
+import { useState, useEffect } from 'react'
+import { getPreciseDistance } from 'geolib';
 import "./Map.css"
 import "leaflet/dist/leaflet.css";
 
 import icon from 'leaflet/dist/images/marker-icon.png';
+import redIcon from './red_marker.png'
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 const center = [34.0689, -118.4452]
 const buildings = buildingData.features
 
-let DefaultIcon = L.icon({
+var DefaultIcon = L.icon({
     iconUrl: icon,
     shadowUrl: iconShadow
 });
 
+var RedIcon = L.icon({
+  iconUrl: redIcon,
+  shadowUrl: iconShadow,
+  iconSize:     [25, 37], // size of the icon
+  shadowSize:   [40, 40], // size of the shadow
+  shadowAnchor: [12, 21],  // the same for the shadow
+  popupAnchor:  [0, 0]
+})
 L.Marker.prototype.options.icon = DefaultIcon;
 
-const LocationMarker = () => {
-
+ function LocationMarker(){
   const [position, setPosition] = useState(null)
+  const [closestBuilding, setClosestBuilding] = useState({})
+
   const map = useMapEvents({
     click(e) {
       setPosition(e.latlng)
+      setClosestBuilding(findClosestBuilding(position.lat, position.lng))
       map.flyTo(e.latlng, map.getZoom())
     },
   })
-  return position === null ? null : (
-    <Marker position={position}>
+  return (position === null) ? null : (
+    <Marker position={position} icon={RedIcon}>
       <Popup>
         <h1>You are here!</h1>
-        <p>coordinates: {position.lat}, {position.lng}</p>
+        <p>Closest Building: {closestBuilding.name} Distance: {closestBuilding.dist} m</p>
       </Popup>
     </Marker>
   )
+}
+
+function findClosestBuilding(lat, lng){
+  var arr,
+  minDist = Infinity,
+  minBuilding = "",
+  itemDistance = 0
+
+  for(let i = 0; i < buildings.length; i++){
+    console.log("ITEM: " + buildings[i].properties.NAME);
+    itemDistance = getPreciseDistance(
+      {latitude: lat, longitude: lng},
+      {latitude: buildings[i].geometry.coordinates[0], longitude: buildings[i].geometry.coordinates[1]}
+    );
+    if(itemDistance < minDist){
+      minDist = itemDistance;
+      minBuilding = buildings[i].properties.NAME;
+    };
+  }
+  console.log("Closest Building: " + minBuilding + "Distance: " + minDist);
+  arr = {name: minBuilding, dist: itemDistance};
+  return arr;
 }
 
 function Map() {
@@ -45,7 +78,6 @@ function Map() {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
-        <LocationMarker />
         {buildingData.features.map(building => (
           <Marker key={building.properties.BUILDING_ID} position={[
             building.geometry.coordinates[0],
@@ -60,6 +92,7 @@ function Map() {
             </Popup>
           </Marker>
         ))}
+        <LocationMarker />
       </MapContainer>
     );
   }
